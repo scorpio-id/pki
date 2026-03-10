b40f302ad5bc79718918648589a9b5f02db67abd
brandon <b.mazey@nyu.edu>
2026-03-10 17:28:35 -0400

    changing config for DB wipe

--- internal/config/config.go
+++ internal/config/config.go
@@ -85,14 +85,14 @@ func NewConfig(s string) Config {
 	}
 	
 	// TODO retrieve content from Kube Secrets using configured file paths if persistence enabled
-	if cfg.Persistence.Enabled {
+	// if cfg.Persistence.Enabled {
 		content, err := os.ReadFile(cfg.Persistence.Path)
 		if err != nil {
 			log.Fatalf("Error reading file: %v", err)
 		}
 
 		cfg.Persistence.Password = string(content)
-	}
+	// }
 
 	return cfg
 }
